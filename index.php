da482b1f8e3711cf1c4023804c66f50435180642
Manpreetx1 <145280989+Manpreetx1@users.noreply.github.com>
2024-04-16 09:25:50 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$title = 'Home';
+require 'include/header.php';
+?>
+<main class="container">
+    <h1>Final Exam</h1>
+    <p>This site is built using PHP and MySQL and hosted on AWS.</p>
+    <p>We're building it for COMP1006 Intro to Web Programming @ Georgian College.</p>
+    <p>Source Code available on
+        <a href="https://github.com/ifotn/comp1006-final-w24.git">GitHub</a>.
+    </p>
+    <p>All game images sourced from
+        <a href="https://wikipedia.org" target="_blank">Wikipedia</a>.
+    </p>
+    <a class="bmi-calulator-io" href="https://100widgets.com/informers/268-body-mass-bmi-calculator.html" data-label_1="BMI CALCULATOR" data-label_2="BMI" data-theme="original" >BMI CALCULATOR</a>
+    
+    <script type="text/javascript" src="//100widgets.com/js_data.php?id=268"></script>
+    
+    <p>Created my logo at <a href="LogoMakr.com" target="_blank">LogoMakr.com</a>.</p>
+</main>
+</body>
+</html>
